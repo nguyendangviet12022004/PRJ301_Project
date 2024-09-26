dd2319ff9631ab3ed1829c1c9fd329ad64597601
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2024-09-26 17:29:39 +0700

    start project

---
+++ ebay/src/java/util/DBHelper.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBHelper implements Serializable{
+    public static Connection makeConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url="jdbc:sqlserver:"
+                + "//localhost:1433"
+                + ";databaseName=Store;encrypt=true;trustServerCertificate=true";
+        Connection con=DriverManager.getConnection(url,"sa","123");
+        return con;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            System.out.println(makeConnection());
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
