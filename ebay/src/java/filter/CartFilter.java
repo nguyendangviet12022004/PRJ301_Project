960f00746038be8e2982c26d4306442ef18cfba6
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2024-10-13 17:22:47 +0700

    add cartFilter nhe may cung

---
+++ ebay/src/java/filter/CartFilter.java
@@ -0,0 +1,51 @@
+package filter;
+
+import constant.IConstant;
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.AccountDTO;
+
+public class CartFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest rq, ServletResponse rp,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) rq;
+        HttpServletResponse response = (HttpServletResponse) rp;
+        HttpSession session = request.getSession();
+        AccountDTO account = (AccountDTO) session.getAttribute("account");
+        String action = request.getParameter("action");
+        if (action == null || action.isEmpty() || action.isBlank()) {
+            action = "read";
+        }
+        switch (action) {
+            case "read":
+                chain.doFilter(rq, rp);
+                break;
+            case "create":
+                if (account != null) {
+                    chain.doFilter(rq, rp);
+                } else {
+                    response.sendRedirect(IConstant.SIGN_IN_FORM);
+                }
+                break;
+            default:
+                if (account == null) {
+                    response.sendRedirect(IConstant.SIGN_IN_FORM);
+                } else if (account.getRole().equals(IConstant.ADMIN)) {
+                    chain.doFilter(rq, rp);
+                } else {
+                    response.sendRedirect(IConstant.HOME_PAGE);
+                }
+        }
+
+    }
+}
